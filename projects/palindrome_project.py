169d1d4be8768d3163ac444f55e973480d39018d
bharath <bharathmuruga9337@gmail.com>
2024-09-12 14:26:28 +0530

    convert lower case

--- projects/palindrome_project.py
+++ projects/palindrome_project.py
@@ -1,8 +1,9 @@
 def check_Palindrome(word):
-    if word==word[::-1]:
+    words=word.lower()
+    if words==words[::-1]:
         print(f"{word} palindrome word")
     else:
-        print(f"{word}not Palindrome word")
+        print(f"{word} not Palindrome word")
 
 word=input("enter the word: ")
 check_Palindrome(word)
